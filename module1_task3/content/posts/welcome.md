42f55e67a5daab05f22f29cae25e5aefef67f888
miguel5219 <5219@holbertonstudents.com>
2023-01-11 12:06:56 -0500

    task 3

---
+++ module1_task3/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome to Awesome Inc."
+date: 2023-01-10T19:01:43-05:00
+draft: true
+---
+
---
+++ module1_task3/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install hugo
+sudo apt install make
+make build
